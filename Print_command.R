756b6050f11379469c5922c0a6bafd7c0dae5387
Vanncg1 <vanncg1@gmail.com>
2021-07-13 15:54:37 -0400

    revised last line explaining procedure to commit and push

---
+++ Print_command.R
@@ -0,0 +1,9 @@
+##inputting simple print command
+
+print("This file was created in RStudio")
+
+##creating second line for print command
+
+print("And now it lives on GitHub")
+
+##in the git tab of the environment quad, check script and select commit.  leave an informative commit comment, click commit, close the small window and clish push.  
